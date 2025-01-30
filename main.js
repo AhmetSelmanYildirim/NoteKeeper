41ae61aa29e30b6c8883984255bafd175473b7e4
AhmetSelmanYildirim <ahmet.selman.yildirimm@gmail.com>
2025-01-31 00:42:33 +0300

    initial: init electron & react

---
+++ main.js
@@ -0,0 +1,42 @@
+import { app, BrowserWindow } from "electron";
+import path from "path";
+import "dotenv/config";
+
+const __dirname = path.resolve();
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+      nodeIntegration: true,
+    },
+  });
+
+  if (process.env.NODE_ENV === "development") {
+    mainWindow.loadURL("http://localhost:5173");
+  } else {
+    mainWindow.loadFile(path.join(__dirname, "./dist/index.html"));
+  }
+
+  mainWindow.on("closed", () => {
+    mainWindow = null;
+  });
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
